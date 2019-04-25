5e9085bd1e4d13a1d66a542229d2193205785a92
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-25 09:17:33 -0700

    added routes to views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './views/LandingPage';
+import WizardFormPage from './views/WizardFormPage';
+import OverviewPage from './views/OverviewPage';
+import ValuationPlaygroundPage from './views/ValuationPlaygroundPage';
+import MortgageCalculatorPage from './views/MortgageCalculatorPage';
+import DesignStudioPage from './views/DesignStudioPage';
+// Note: onUpdate sends the user back to the top of the page on route switch.
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Switch>
+        <Route onUpdate={window.scrollTo(0, 0)} path="/" exact component={LandingPage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/wizardform" component={WizardFormPage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/overview" component={OverviewPage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/valuation-playground" component={ValuationPlaygroundPage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/mortgage-calculator" component={MortgageCalculatorPage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/design-studio" component={DesignStudioPage} />
+      </Switch>
     </div>
   );
 }
