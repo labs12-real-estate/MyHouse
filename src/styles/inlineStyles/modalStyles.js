940b9ee335992843c35baeb8d18cf07e6a88c23a
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-25 16:58:05 -0700

    added object for styling modal overlay and size

---
+++ src/styles/inlineStyles/modalStyles.js
@@ -0,0 +1,17 @@
+export const loginModalStyle = () => {
+  return {
+    overlay: {
+      backgroundColor: 'none'
+    },
+    content: {
+      position: 'relative',
+      top: '20%',
+      left: '76%',
+      background: '#fff',
+      outline: 'none',
+      padding: '0px',
+      width: '260px',
+      overflow: 'none'
+    }
+  };
+};
