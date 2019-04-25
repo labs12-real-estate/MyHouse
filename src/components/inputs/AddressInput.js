9d484298547b1c0d431524d873395519c1d35923
David Lam <Davidlamsgn@gmail.com>
2019-04-25 14:07:10 -0700

    format AddressInput.js with Prettier

--- src/components/inputs/AddressInput.js
+++ src/components/inputs/AddressInput.js
@@ -2,107 +2,94 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class AddressInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      predictions: []
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            address: '',
-            predictions: [],
-        }
+    // Variables for Google Place API
+    this.location = '43.3148,-85.6024'; // Latitude, longtitude of Michigan
+    this.types = 'address';
+    this.key = 'AIzaSyBQG-Y3BtowkEvTBq3dPPROa-GuMm1Rfpk';
+    this.sessiontoken = '';
+  }
 
-        // Variables for Google Place API
-        this.location = '43.3148,-85.6024'; // Latitude, longtitude of Michigan
-        this.types = 'address';
-        this.key = 'AIzaSyBQG-Y3BtowkEvTBq3dPPROa-GuMm1Rfpk';
-        this.sessiontoken = '';
-    }
-    
-    // Free api to generate version 4 UUID session token as Google recommends
-    generateSessionToken() {
-        axios
-                .get('https://www.uuidgenerator.net/api/version4')
-                .then(res => {
-                    this.sessiontoken = res.data;
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-    }
+  // Free api to generate version 4 UUID session token as Google recommends
+  generateSessionToken() {
+    axios
+      .get('https://www.uuidgenerator.net/api/version4')
+      .then(res => {
+        this.sessiontoken = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    componentDidMount(){
+  componentDidMount() {
+    // Generate session token after page loaded
+    this.generateSessionToken();
+  }
 
-        // Generate session token after page loaded
-        this.generateSessionToken();
-    }
+  handleInputChange = e => {
+    e.persist();
+    let string = e.target.value;
+    this.setState(prevState => ({
+      ...prevState,
+      [e.target.name]: string
+    }));
 
-    handleInputChange = e => {
-
-        e.persist();
-        let string = e.target.value;
-        this.setState(prevState => ({
+    // Autocomplete request is called every few keystrokes
+    if ([3, 6, 10, 15].includes(string.length)) {
+      console.log(this.sessiontoken);
+      const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${string}&types=${
+        this.types
+      }&location=${this.location}&radius=500&key=${this.key}&sessiontoken=${this.sessiontoken}`;
+      axios
+        .get(url)
+        .then(res => {
+          this.setState(prevState => ({
             ...prevState,
-            [e.target.name]: string
-        }));
-
-        // Autocomplete request is called every few keystrokes
-        if ([3, 6, 10, 15].includes(string.length)) {
-            console.log(this.sessiontoken);
-            const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${string}&types=${this.types}&location=${this.location}&radius=500&key=${this.key}&sessiontoken=${this.sessiontoken}`;
-            axios
-                .get(url)
-                .then(res => {
-                    this.setState(prevState => ({
-                        ...prevState,
-                        predictions: res.data.predictions
-                    }));
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
+            predictions: res.data.predictions
+          }));
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
+  };
 
-    getValue = e => {
+  getValue = e => {
+    e.preventDefault();
+    this.generateSessionToken(); // New session token when search button clicked
+  };
 
-        e.preventDefault()
-        this.generateSessionToken(); // New session token when search button clicked
-    }
+  fillAddress = (address, e) => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      ...prevState,
+      address: address
+    }));
+  };
 
-    fillAddress = (address, e) => {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.getValue}>
+          <input onChange={this.handleInputChange} placeholder="address" value={this.state.address} name="address" />
+          <button className="form-button">Search</button>
+        </form>
 
-        e.preventDefault();
-        this.setState(prevState => ({
-            ...prevState,
-            address: address
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-
-                <form onSubmit={this.getValue}>
-                    <input
-                        onChange={this.handleInputChange}
-                        placeholder="address"
-                        value={this.state.address}
-                        name="address"
-                    />
-                    <button className="form-button">Search</button>
-                </form>
-
-                {this.state.predictions.map(prediction => (
-
-                    <button 
-                        key={prediction.id}
-                        onClick={e => this.fillAddress(prediction.description, e)}
-                    >
-                        {prediction.description}
-                    </button>
-                ))}
-            </div>
-        );
-    }
+        {this.state.predictions.map(prediction => (
+          <button key={prediction.id} onClick={e => this.fillAddress(prediction.description, e)}>
+            {prediction.description}
+          </button>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default AddressInput;
