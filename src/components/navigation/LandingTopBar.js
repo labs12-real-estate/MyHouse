f509736ea4dce89a13bb9fc08e8ff8285dbf738c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-25 13:52:47 -0700

    added home icon with link to '/'

--- src/components/navigation/LandingTopBar.js
+++ src/components/navigation/LandingTopBar.js
@@ -5,7 +5,11 @@ import Button from '../buttons/Button';
 function LandingTopBar() {
   return (
     <div className="landing_nav_container">
-      <div className="landing_nav_home_icon">HomeImg</div>
+      <div className="landing_nav_home_icon">
+        <Link to="/">
+          <i className="fas fa-home fa-2x" />
+        </Link>
+      </div>
       <div className="landing_nav_item_container">
         <Link to="/">Home</Link>
         <Link to="#">Team</Link>
