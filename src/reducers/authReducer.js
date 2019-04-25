c89ff103f4349610b64658dc2ce27bdeae8bda6c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-25 16:55:22 -0700

    added case for login_modal boolean

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,5 +1,15 @@
-export const authReducer = (state = {}, action) => {
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/index';
+
+const initialState = {
+  isOpen: false
+};
+
+export const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case OPEN_MODAL:
+      return { ...state, isOpen: action.payload };
+    case CLOSE_MODAL:
+      return { ...state, isOpen: action.payload };
     default:
       return state;
   }
