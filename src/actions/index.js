eedc37fc6c6de060e18fd573c8c443dabefae72d
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-25 10:37:05 -0700

    added example to show how actions index is used.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+// Below is an example of how to use index.js.
+
+// exampleAction Types
+
+export const EXAMPLE_TYPE = 'EXAMPLE_TYPE';
+
+// authAction Types
+
+// userAction Types
+
+// houseAction Types
