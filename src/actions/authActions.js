e5f211786b9440aaeb0c1ecdfe0c003d70aff2d1
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-25 16:55:16 -0700

    added actions for login_modal boolean

---
+++ src/actions/authActions.js
@@ -0,0 +1,17 @@
+import { OPEN_MODAL, CLOSE_MODAL } from './index';
+
+export const openModal = e => {
+  e.preventDefault();
+  return {
+    type: OPEN_MODAL,
+    payload: true
+  };
+};
+
+export const closeModal = e => {
+  e.preventDefault();
+  return {
+    type: CLOSE_MODAL,
+    payload: false
+  };
+};
