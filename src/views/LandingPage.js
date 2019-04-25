eb6cf17769dc81297768e40143030b78d8a4335e
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-04-25 15:01:25 -0700

    Adds cloud to landing page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -3,10 +3,10 @@ import LandingTopBar from '../components/navigation/LandingTopBar';
 
 function LandingPage() {
   return (
-    <div>
+    <div className="landing_body">
       <LandingTopBar />
       <h1>LandingPage</h1>
-      <div className="cloud-top" />
+      <div className="cloud_top" />
     </div>
   );
 }
