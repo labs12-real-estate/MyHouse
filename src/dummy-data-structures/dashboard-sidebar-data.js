d04fe8224576c536be035c2cdf0585ce046d0406
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-26 15:42:26 -0700

    added a dummy array for dashboard SideNavBar

---
+++ src/dummy-data-structures/dashboard-sidebar-data.js
@@ -0,0 +1,32 @@
+export const dashboardSidebarData = [
+  {
+    navTitle: 'Overview',
+    path: '/overview',
+    icon: 'far fa-newspaper fa-3x',
+    id: Math.random()
+  },
+  {
+    navTitle: 'MyHouse',
+    path: '/myhouse',
+    icon: 'fas fa-door-open fa-3x',
+    id: Math.random()
+  },
+  {
+    navTitle: 'Valuation Playground',
+    path: '/valuation-playground',
+    icon: 'fas fa-chart-line fa-3x',
+    id: Math.random()
+  },
+  {
+    navTitle: 'Mortgage Calculator',
+    path: '/mortgage-calculator',
+    icon: 'fas fa-calculator fa-3x',
+    id: Math.random()
+  },
+  {
+    navTitle: 'Design Studio',
+    path: '/design-studio',
+    icon: 'fas fa-swatchbook fa-3x',
+    id: Math.random()
+  }
+];
