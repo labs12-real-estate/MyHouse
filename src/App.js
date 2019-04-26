0d0c4c605702deffd41026b07ff62f796ff7882d
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-26 15:40:59 -0700

    added MyHousePage route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import LandingPage from './views/LandingPage';
 import WizardFormPage from './views/WizardFormPage';
 import OverviewPage from './views/OverviewPage';
+import MyHousePage from './views/MyHousePage';
 import ValuationPlaygroundPage from './views/ValuationPlaygroundPage';
 import MortgageCalculatorPage from './views/MortgageCalculatorPage';
 import DesignStudioPage from './views/DesignStudioPage';
@@ -15,6 +16,7 @@ function App() {
         <Route onUpdate={window.scrollTo(0, 0)} path="/" exact component={LandingPage} />
         <Route onUpdate={window.scrollTo(0, 0)} path="/wizard-form" component={WizardFormPage} />
         <Route onUpdate={window.scrollTo(0, 0)} path="/overview" component={OverviewPage} />
+        <Route onUpdate={window.scrollTo(0, 0)} path="/myhouse" component={MyHousePage} />
         <Route onUpdate={window.scrollTo(0, 0)} path="/valuation-playground" component={ValuationPlaygroundPage} />
         <Route onUpdate={window.scrollTo(0, 0)} path="/mortgage-calculator" component={MortgageCalculatorPage} />
         <Route onUpdate={window.scrollTo(0, 0)} path="/design-studio" component={DesignStudioPage} />
