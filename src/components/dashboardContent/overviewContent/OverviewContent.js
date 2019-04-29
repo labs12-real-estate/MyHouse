7f180a78617cb7ee2294bbbf8b5da0283a23d62c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-29 09:56:09 -0700

    Made design changes to dashboard Sidebar

--- src/components/dashboardContent/overviewContent/OverviewContent.js
+++ src/components/dashboardContent/overviewContent/OverviewContent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function OverviewContent() {
   return (
-    <div className="overview_content_content">
+    <div className="overview_content_container">
       <h1>OverviewContent</h1>
     </div>
   );
