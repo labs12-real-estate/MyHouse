d2ee5d40d501a3eb762f11bdc71edb1c8cc60be1
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-29 15:50:56 -0700

    added dummy data for valuation playground

---
+++ src/dummy-data-structures/valuation-playground-upgrade-list.js
@@ -0,0 +1,188 @@
+export const upgradeList = {
+  counterTops: [
+    {
+      name: 'Laminate',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Granite/Concrete',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Marble/Quartz',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    }
+  ],
+  roof: [
+    {
+      name: 'Asphalt',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Metal',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Solar',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Concrete/Clay',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Cedar/Shake',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    }
+  ],
+  siding: [
+    {
+      name: 'Stucco',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Cedar/Shake',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Vinyl',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Brick',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Wood Siding',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Fiber Cement',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    }
+  ],
+  AC: [
+    {
+      name: '2 Ton',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: '2 1/2 Ton',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: '3 Ton',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: '4 Ton',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    }
+  ],
+  furnace: [
+    {
+      name: '60,000 BTU',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: '70,000 BTU',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: '80,000 BTU',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    }
+  ],
+  flooring: [
+    {
+      name: 'Carpet',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Tile',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Engineered Wood/Laminate',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Hardwood',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Concrete',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    }
+  ],
+  addingSqFt: [
+    {
+      name: 'Loft',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Furnished Basement',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Bathroom',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Bedroom',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    }
+  ],
+  painting: [
+    {
+      name: 'Full Exterior Paint',
+      upgrade_cost: 1200,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Full Interior Paint',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Repaint Front Door',
+      upgrade_cost: 3500,
+      ROI_percentage: 2.5
+    },
+    {
+      name: 'Replace Front Door',
+      upgrade_cost: 2500,
+      ROI_percentage: 2.5
+    }
+  ]
+};
