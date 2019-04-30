46e06aa496eb4293a3a3bec1935fd8bac9a22b33
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-30 10:13:49 -0700

    Display house data (as raw JSON) in OverviewPage

--- src/views/OverviewPage.js
+++ src/views/OverviewPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import DashboardTopBar from '../components/navigation/DashboardTopBar';
 import SideBar from '../components/navigation/SideBar';
 import OverviewContent from '../components/dashboardContent/overviewContent/OverviewContent';
+import { graphqlOperation } from 'aws-amplify';
+import { Authenticator, Connect, SignIn } from 'aws-amplify-react';
+import { listHouses } from '../graphql/queries';
 
 function OverviewPage() {
   return (
@@ -9,7 +12,14 @@ function OverviewPage() {
       <DashboardTopBar />
       <div className="dashboard_page_container">
         <SideBar />
-        <OverviewContent />
+        <div className="dashboard_content_container">
+          <Authenticator />
+          <Connect query={graphqlOperation(listHouses)}>
+            {({ loading, data, errors }) =>
+              loading ? <h3>Loading...</h3> : data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <pre>{JSON.stringify(errors, null, 2)}</pre>
+            }
+          </Connect>
+        </div>
       </div>
     </div>
   );
