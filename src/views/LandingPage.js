b5f45225bde611acc754d632f5887963c928720f
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-04-29 17:06:12 -0700

    Imports, displays, and styles macbook image

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LandingTopBar from '../components/navigation/LandingTopBar';
 import housePng from '../assets/images/isometric-house.png';
+import macBook from '../assets/images/macbook.png';
 import AddressInput from '../components/inputs/AddressInput';
 import Footer from '../components/footer/Footer';
 
@@ -8,6 +9,7 @@ function LandingPage() {
   return (
     <div className="landing_body">
       <LandingTopBar />
+
       <div className="hero">
         <h1>MyHouse</h1>
         <p>
@@ -16,16 +18,23 @@ function LandingPage() {
         </p>
         <AddressInput />
       </div>
+
       <div className="cloud_top">
         <img src={housePng} alt="isometric-house" />
       </div>
-      <div className="landing_mid">
+
+      <section className="landing_mid">
         <h2>Skip the Middleman</h2>
         <p>
           Coupled with the best machine learning algorithms in real estate technology, MyHouse’s data provides homeowners and buyers the most accurate decision
           making information on the market
         </p>
-      </div>
+      </section>
+
+      <section className="macbook">
+        <img src={macBook} alt="macbook with demo" />
+      </section>
+
       <Footer />
     </div>
   );
