01d6b2e3355430f2a9775a40e8f02a051b4cccf7
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-30 09:46:56 -0700

    Add autogenerated GraphQL queries/mutations/config

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,90 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateHouse = `subscription OnCreateHouse {
+  onCreateHouse {
+    id
+    photos
+    address {
+      street
+      city
+      state
+      zip
+    }
+    parcelData {
+      squareFootage
+      numBedrooms
+      numBathrooms
+      lotAcreage
+      yearBuilt
+      garageSize
+      architecturalStyle
+      waterfront
+    }
+    userProvided {
+      countertops
+      acAge
+      furnaceAge
+    }
+    valuation
+  }
+}
+`;
+export const onUpdateHouse = `subscription OnUpdateHouse {
+  onUpdateHouse {
+    id
+    photos
+    address {
+      street
+      city
+      state
+      zip
+    }
+    parcelData {
+      squareFootage
+      numBedrooms
+      numBathrooms
+      lotAcreage
+      yearBuilt
+      garageSize
+      architecturalStyle
+      waterfront
+    }
+    userProvided {
+      countertops
+      acAge
+      furnaceAge
+    }
+    valuation
+  }
+}
+`;
+export const onDeleteHouse = `subscription OnDeleteHouse {
+  onDeleteHouse {
+    id
+    photos
+    address {
+      street
+      city
+      state
+      zip
+    }
+    parcelData {
+      squareFootage
+      numBedrooms
+      numBathrooms
+      lotAcreage
+      yearBuilt
+      garageSize
+      architecturalStyle
+      waterfront
+    }
+    userProvided {
+      countertops
+      acAge
+      furnaceAge
+    }
+    valuation
+  }
+}
+`;
