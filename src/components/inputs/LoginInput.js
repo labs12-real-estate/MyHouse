444816ddae4e346776887424d27c4ea9da8b733a
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-30 17:14:28 -0700

    Create LoginInput

---
+++ src/components/inputs/LoginInput.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { useForm } from '../../helper-functions/form-logic-functions';
+import { signIn } from '../../actions/authActions';
+import Button from '../buttons/Button';
+
+const initialCreds = {
+  username: '',
+  password: ''
+};
+
+function LoginInput({ signIn }) {
+  const [creds, handleChange] = useForm(initialCreds);
+  return (
+    <form onSubmit={e => signIn(creds, e)}>
+      <div className="login_inputs_container">
+        <label className="login_label">Username</label>
+        <input name="username" value={creds.username} onChange={handleChange} className="login_input" type="text" />
+        <label className="login_label">Password</label>
+        <input name="password" value={creds.password} onChange={handleChange} className="login_input" type="password" />
+        <Button buttonStyle="modal_login_button" buttonText="Login" />
+      </div>
+    </form>
+  );
+}
+
+export default connect(
+  null,
+  { signIn }
+)(LoginInput);
