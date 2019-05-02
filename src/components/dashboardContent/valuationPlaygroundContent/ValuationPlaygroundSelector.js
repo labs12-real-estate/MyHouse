1387571de2715901662d6d26080e66d109f793f8
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-01 23:13:54 -0700

    Refactored list of options to use a .map function

--- src/components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundSelector.js
+++ src/components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundSelector.js
@@ -14,13 +14,9 @@ function ValuationPlaygroundSelector({ currentUpgrade, upgradeList, setCurrentUp
     <div className="valuation_playground_selector_container">
       <h1 className="valuation_playground_upgradeType_title">{upgradeType}</h1>
       <select value={selectedValue()} onChange={changeHandler} className="valuation_playground_selector">
-        {upgradeList[0] && <option value="">{upgradeList[0].name}</option>}
-        {upgradeList[1] && <option>{upgradeList[1].name}</option>}
-        {upgradeList[2] && <option>{upgradeList[2].name}</option>}
-        {upgradeList[3] && <option>{upgradeList[3].name}</option>}
-        {upgradeList[4] && <option>{upgradeList[4].name}</option>}
-        {upgradeList[5] && <option>{upgradeList[5].name}</option>}
-        {upgradeList[6] && <option>{upgradeList[6].name}</option>}
+        {upgradeList.map((upgrade, index) => {
+          return index === 0 ? <option value="">{upgrade.name}</option> : <option>{upgrade.name}</option>;
+        })}
       </select>
     </div>
   );
