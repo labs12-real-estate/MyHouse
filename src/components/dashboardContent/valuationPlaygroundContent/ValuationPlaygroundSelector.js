7132f470c92fff145ff634b2d44623fe53fa034d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-02 14:45:54 -0700

    Highlight effect on selected upgrade

--- src/components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundSelector.js
+++ src/components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundSelector.js
@@ -10,15 +10,12 @@ function ValuationPlaygroundSelector({ currentUpgrade, upgradeList, setCurrentUp
     return upgradeList.some(upgrade => upgrade.name === currentUpgrade.name) ? undefined : '';
   };
 
-  const upgradeNames = upgradeList.map(i => {
-    // return i.name.includes('Select');
-    return i.name;
-  });
-  console.log(upgradeNames);
-  console.log(upgradeNames.includes(currentUpgrade.name));
+  const upgradeNames = upgradeList.map(i => i.name);
+
+  const highlightSelected = { opacity: upgradeNames.includes(currentUpgrade.name) ? 1 : 0.4 };
 
   return (
-    <div className="valuation_playground_selector_container" style={{ background: upgradeNames.includes(currentUpgrade.name) ? 'white' : 'grey' }}>
+    <div className="valuation_playground_selector_container" style={highlightSelected}>
       <h1 className="valuation_playground_upgradeType_title">{upgradeType}</h1>
       <select value={selectedValue()} onChange={changeHandler} className="valuation_playground_selector">
         {upgradeList.map((upgrade, index) => {
