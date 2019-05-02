0dcb6f7c88498252d4f3ee6522d3890fcef18a42
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-01 17:32:15 -0700

    Add Confirmation page to handle end of form

---
+++ src/components/wizardForm/WizardFormConfirmation.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import Button from '../../components/buttons/Button';
+
+const NULL = 'NULL';
+
+function WizardFormConfirmation({ questions, handleAnswer, handleSubmit }) {
+  const handleChange = qIx => e => {
+    const oIx = e.target.value === NULL ? null : +e.target.value;
+    handleAnswer(qIx)(oIx)(e);
+  };
+  return (
+    <div>
+      <h2>WizardFormConfirmation</h2>
+      <p>Please review your answers before submitting</p>
+      {questions.map(({ title, options, selected }, qIx) => (
+        <Fragment key={qIx}>
+          <label htmlFor={title}>{title}</label>
+          <select onChange={handleChange(qIx)} value={selected || NULL} id={title}>
+            <option value={NULL}>Not applicable</option>
+            {options.map((opt, oIx) => (
+              <option key={oIx} value={oIx}>
+                {opt}
+              </option>
+            ))}
+          </select>
+        </Fragment>
+      ))}
+      <Button clickEvent={handleSubmit} buttonStyle="" buttonText="Submit" />
+    </div>
+  );
+}
+
+export default WizardFormConfirmation;
