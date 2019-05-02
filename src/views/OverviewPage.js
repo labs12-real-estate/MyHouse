aa3b8024a82afab35f64520a3d6b2c534912a758
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-01 22:04:01 -0700

    removed AWS components

--- src/views/OverviewPage.js
+++ src/views/OverviewPage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import DashboardTopBar from '../components/navigation/DashboardTopBar';
 import SideBar from '../components/navigation/SideBar';
-// import OverviewContent from '../components/dashboardContent/overviewContent/OverviewContent';
-import { graphqlOperation } from 'aws-amplify';
-import { Authenticator, Connect } from 'aws-amplify-react';
-import { listHouses } from '../graphql/queries';
+import OverviewContent from '../components/dashboardContent/overviewContent/OverviewContent';
 
 function OverviewPage() {
   return (
@@ -13,12 +10,7 @@ function OverviewPage() {
       <div className="dashboard_page_container">
         <SideBar />
         <div className="dashboard_content_container">
-          <Authenticator />
-          <Connect query={graphqlOperation(listHouses)}>
-            {({ loading, data, errors }) =>
-              loading ? <h3>Loading...</h3> : data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <pre>{JSON.stringify(errors, null, 2)}</pre>
-            }
-          </Connect>
+          <OverviewContent />
         </div>
       </div>
     </div>
