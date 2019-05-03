92583f42426f1aa2bfcb671ad0b3e1f61c0dda0c
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-03 15:54:35 -0700

    Styles house profile form title

--- src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function HouseProfileForm(props) {
   return (
-    <div>
-      <h2>{props.title}</h2>
+    <div className="house_profile_form">
+      <div className="house_profile_form_title">
+        <h2>{props.title}</h2>
+        <i className="fas fa-pen" />
+      </div>
       <p>{props.content}</p>
     </div>
   );
