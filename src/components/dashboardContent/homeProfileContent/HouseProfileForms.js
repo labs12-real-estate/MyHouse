72e7a86ac2306a994cb889138ac34ac46e838393
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-03 15:31:35 -0700

    HouseProfileForms started

---
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HouseProfileForms() {
+  return (
+    <div className="house_profile_forms_container">
+      <div className="house_profile_forms_left">HouseProfileForms</div>
+      <div className="house_profile_forms_right">HouseProfileForms</div>
+    </div>
+  );
+}
+
+export default HouseProfileForms;
--- src/components/dashboardContent/homeProfileContent/MyHouseContent.js
+++ src/components/dashboardContent/homeProfileContent/MyHouseContent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Backdrop from './Backdrop';
 import UserProfileSection from './UserProfileSection';
+import HouseProfileForms from './HouseProfileForms';
 
 function MyHouseContent() {
   return (
     <div className="dashboard_content_container">
       <Backdrop />
       <UserProfileSection />
+      <HouseProfileForms />
     </div>
   );
 }
