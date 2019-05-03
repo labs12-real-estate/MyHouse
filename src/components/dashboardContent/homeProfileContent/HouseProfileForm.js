8d5d9a22876a294fd7f592bf8fe4257a39d9dc6e
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-03 15:43:32 -0700

    Renders dummy data on house profile page

---
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HouseProfileForm(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+export default HouseProfileForm;
--- src/components/dashboardContent/homeProfileContent/HouseProfileForms.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForms.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { houseProfileData } from '../../../dummy-data-structures/house-profile-dummy-data';
+import HouseProfileForm from './HouseProfileForm';
 
 function HouseProfileForms() {
   return (
     <div className="house_profile_forms_container">
-      <div className="house_profile_forms_left">HouseProfileForms</div>
+      <div className="house_profile_forms_left">
+        {houseProfileData.map((i, index) => (
+          <HouseProfileForm key={index} title={i.title} content={i.content} />
+        ))}
+      </div>
       <div className="house_profile_forms_right">HouseProfileForms</div>
     </div>
   );
