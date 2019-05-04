6a9f919857a6e498582c1d0571d3ba326f49ca49
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-03 19:09:26 -0700

    Creates more dummy data for house profile

--- src/dummy-data-structures/house-profile-dummy-data.js
+++ src/dummy-data-structures/house-profile-dummy-data.js
@@ -1,3 +1,5 @@
+import { Agent } from 'https';
+
 export const houseProfileData = [
   {
     title: 'House Bio',
@@ -14,4 +16,17 @@ export const houseProfileData = [
     content:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, pariatur provident id quo sunt fugit corrupti animi praesentium dolore, quasi, nobis quas cumque doloremque. Et debitis quidem nobis fuga vel voluptate magni aut est pariatur hic reprehenderit perspiciatis libero illum, vero repellendus cumque nulla facilis at fugit architecto non sed laborum! Omnis, maiores dicta. In voluptates nisi asperiores recusandae saepe.'
   }
+];
+
+export const houseProfileDataRight = [
+  {
+    title: 'Finish Data',
+    options: [
+      { countertops: ['laminate', 'granite', 'marble'] },
+      { roofAge: ['0-3', '4-9', '10-14', '15+'] },
+      { kitchenFlooringType: ['tile', 'concrete'] },
+      { furnaceAge: ['0-3', '4-9', '10-14', '15+'] }
+    ]
+  },
+  { title: 'Parcel Data', options: ['data 1', 'data 2', 'data 3', 'data 4', 'data 5', 'data 6'] }
 ];
