78847e37ef9a5f093e98ce8eec1ddcd6a563b90a
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-06 15:51:41 -0700

    cleaned up LandingPage.js so JSX is broken into components

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,100 +1,20 @@
 import React from 'react';
 import LandingTopBar from '../components/navigation/LandingTopBar';
-import housePng from '../assets/images/isometric-house.png';
-import macBook from '../assets/images/macbook.png';
-import laptop from '../assets/icons/laptop.svg';
-import statistics from '../assets/icons/statistics.svg';
-import calculator from '../assets/icons/calculator.svg';
-import AddressInput from '../components/inputs/AddressInput';
-import MarketingCard from '../components/cards/MarketingCard';
-import Button from '../components/buttons/Button';
 import Footer from '../components/footer/Footer';
-import ReviewCard from '../components/cards/ReviewCard';
+import LandingHero from '../components/landingPageContent/LandingHero';
+import LandingMiddle from '../components/landingPageContent/LandingMiddle';
+import LandingReviews from '../components/landingPageContent/LandingReviews';
+import LandingBottomCTA from '../components/landingPageContent/LandingBottomCTA';
 
 function LandingPage() {
   return (
     <div className="landing_body">
       <LandingTopBar />
-
-      <div className="hero">
-        <h1>MyHouse</h1>
-        <p>
-          We have the most precise home
-          <br /> valuation techniques in the industry
-        </p>
-        <AddressInput />
-      </div>
-
-      <div className="cloud_top">
-        <img src={housePng} alt="isometric-house" />
-      </div>
-
-      <section className="landing_mid">
-        <h2>Skip the Middleman</h2>
-        <p>
-          Coupled with the best machine learning algorithms in real estate technology, MyHouse’s data provides homeowners and buyers the most accurate decision
-          making information on the market
-        </p>
-      </section>
-
-      <section className="macbook">
-        <img src={macBook} alt="macbook with demo" />
-      </section>
-
-      <div className="landing_cloud_2" />
-
-      <section className="landing_mid_grey">
-        <h2>Why MyHouse?</h2>
-        <div className="marketing_cards">
-          <MarketingCard
-            icon={laptop}
-            title="Customized"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          />
-          <MarketingCard
-            icon={calculator}
-            title="Transparent"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          />
-          <MarketingCard
-            icon={statistics}
-            title="Simple"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          />
-        </div>
-      </section>
-
+      <LandingHero />
+      <LandingMiddle />
       <div className="landing_cloud_3" />
-
-      <section className="landing_mid">
-        <div className="reviews">
-          <h2>Reviews</h2>
-          <div className="review_cards">
-            <ReviewCard
-              comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              author="Lorem Ipsum"
-            />
-            <ReviewCard
-              comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              author="Lorem Ipsum"
-            />
-            <ReviewCard
-              comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              author="Lorem Ipsum"
-            />
-            <ReviewCard
-              comment="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              author="Lorem Ipsum"
-            />
-          </div>
-        </div>
-      </section>
-
-      <section className="landing_cta_bottom">
-        <div className="landing_house_bg" />
-        <p>See your home's value</p>
-        <Button buttonText="Get Started" buttonStyle="cta_btn_bottom" />
-      </section>
+      <LandingReviews />
+      <LandingBottomCTA />
       <Footer />
     </div>
   );
