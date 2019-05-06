682adf050d32078819e7ef3bc9ee4d885c3a7997
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-05 19:37:07 -0700

    Temporary: override PrivateRoute

--- src/components/privateRoute/PrivateRoute.js
+++ src/components/privateRoute/PrivateRoute.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const checkLocalStorage = localStorage.getItem('aws.cognito.identity-id.us-east-2:0b1cd1b4-9317-4db9-8797-88b669850445');
-  return <Route {...rest} render={props => (checkLocalStorage ? <Component {...props} /> : <Redirect to="/" />)} />;
+  // const checkLocalStorage = localStorage.getItem('aws.cognito.identity-id.us-east-2:0b1cd1b4-9317-4db9-8797-88b669850445');
+  return <Route {...rest} render={props => (true ? <Component {...props} /> : <Redirect to="/" />)} />;
 }
 
 export default PrivateRoute;
