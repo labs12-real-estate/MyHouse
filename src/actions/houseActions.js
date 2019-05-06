677ddc18f8f549a367f0dac183fdc98a80172892
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-06 10:41:25 -0700

    Remove getHouse action, types

--- src/actions/houseActions.js
+++ src/actions/houseActions.js
@@ -1,7 +1,6 @@
-import { GET_HOUSE_FETCH, GET_HOUSE_SUCCESS, GET_HOUSE_FAIL, MAKE_HOUSE_FETCH, MAKE_HOUSE_SUCCESS, MAKE_HOUSE_FAIL, SET_CURRENT_VALUE } from './index';
+import { MAKE_HOUSE_FETCH, MAKE_HOUSE_SUCCESS, MAKE_HOUSE_FAIL } from './index';
 import { API, graphqlOperation } from 'aws-amplify';
 import { makeUser } from './usersActions';
-import { listHouses } from '../graphql/queries';
 import { createHouse } from '../graphql/mutations';
 
 export const makeHouse = ({ id, houseInput }, history) => dispatch => {
@@ -10,35 +9,9 @@ export const makeHouse = ({ id, houseInput }, history) => dispatch => {
     .then(({ data }) => {
       const house = data.createHouse;
       dispatch({ type: MAKE_HOUSE_SUCCESS, payload: house });
-      makeUser(id)(dispatch).then(() => {
-        history.push('/overview');
-      });
+      makeUser(id, history)(dispatch);
     })
     .catch(error => {
       dispatch({ type: MAKE_HOUSE_FAIL, payload: error });
-    });
-};
-
-export const getHouse = () => dispatch => {
-  dispatch({
-    type: GET_HOUSE_FETCH
-  });
-  return API.graphql(graphqlOperation(listHouses))
-    .then(({ data }) => {
-      const house = data.listHouses.items[0];
-      dispatch({
-        type: GET_HOUSE_SUCCESS,
-        payload: house
-      });
-      dispatch({
-        type: SET_CURRENT_VALUE,
-        payload: house.valuation
-      });
-    })
-    .catch(error => {
-      dispatch({
-        type: GET_HOUSE_FAIL,
-        payload: error
-      });
     });
 };
