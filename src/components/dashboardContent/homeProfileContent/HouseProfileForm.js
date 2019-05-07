112ffc697303142c68e1a1b3fd1d04ad3e28b361
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-07 10:03:13 -0700

    Crates editMode hook

--- src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function HouseProfileForm(props) {
+  const [editMode, setEditMode] = useState(false);
+
   return (
     <div className="house_profile_form">
       <div className="house_profile_form_title">
-        <h2>{props.title}</h2>
-        <i className="fas fa-pen" />
+        <h2>
+          {props.title} <span style={{ color: 'red' }}>{`${editMode}`}</span>
+        </h2>
+        <i className="fas fa-pen" onClick={() => setEditMode(!editMode)} />
       </div>
       <p>{props.content}</p>
     </div>
