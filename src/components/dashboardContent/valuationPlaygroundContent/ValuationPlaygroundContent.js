70896b9a7c6b421ec229912f2136110c84fc064b
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-07 13:50:21 -0700

    added conditional render for upgradelist component

--- src/components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundContent.js
+++ src/components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ValuationPlaygroundJumbo from '../../jumbotrons/ValuationPlaygroundJumbo';
 import ValuationPlaygroundUpgradeLists from './ValuationPlaygroundUpgradeLists';
+import ValuationPlaygroundMobileUpgradeList from './ValuationPlaygroundMobileUpgradeList';
 import { useWindowWidth } from '../../../helper-functions/display-functions';
 
 function ValuationPlaygroundContent(house) {
@@ -8,7 +9,7 @@ function ValuationPlaygroundContent(house) {
     <div className="dashboard_content_container">
       <h1 className="dashboard_content_title">Valuation Playground</h1>
       <div className="valuation_playground_content_container">
-        <ValuationPlaygroundUpgradeLists />
+        {useWindowWidth() <= 1200 ? <ValuationPlaygroundMobileUpgradeList /> : <ValuationPlaygroundUpgradeLists />}
         <ValuationPlaygroundJumbo {...house} />
       </div>
     </div>
