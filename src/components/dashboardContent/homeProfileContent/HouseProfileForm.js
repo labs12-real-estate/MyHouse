e7dea552319f7bf30d3b99b688e5ab675825a38f
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-07 10:18:29 -0700

    Coniditionally renders edit/save icons

--- src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 function HouseProfileForm(props) {
-  const [editMode, setEditMode] = useState(false);
+  const [editing, setEditing] = useState(false);
   const [content, setContent] = useState(props.content);
 
   return (
     <div className="house_profile_form">
       <div className="house_profile_form_title">
         <h2>
-          {props.title} <span style={{ color: 'red' }}>{`${editMode}`}</span>
+          {props.title} <span style={{ color: 'red' }}>{`${editing}`}</span>
         </h2>
-        <i className="fas fa-pen" onClick={() => setEditMode(!editMode)} />
+
+        {!editing ? <i className="fas fa-pen" onClick={() => setEditing(!editing)} /> : <i className="fas fa-check" style={{ color: 'green' }} />}
       </div>
 
-      {!editMode ? <p>{props.content}</p> : <textarea value={content} onChange={e => setContent(e.target.value)} style={{ width: '100%', height: '300px' }} />}
+      {!editing ? <p>{props.content}</p> : <textarea value={content} onChange={e => setContent(e.target.value)} style={{ width: '100%', height: '300px' }} />}
     </div>
   );
 }
