d907de2e4d77529201231448cac26ece6dfab055
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-07 10:15:05 -0700

    onChange handler

--- src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function HouseProfileForm(props) {
   const [editMode, setEditMode] = useState(false);
+  const [content, setContent] = useState(props.content);
 
   return (
     <div className="house_profile_form">
@@ -12,7 +13,7 @@ function HouseProfileForm(props) {
         <i className="fas fa-pen" onClick={() => setEditMode(!editMode)} />
       </div>
 
-      {!editMode ? <p>{props.content}</p> : <textarea value={props.content} style={{ width: '100%', height: '300px' }} />}
+      {!editMode ? <p>{props.content}</p> : <textarea value={content} onChange={e => setContent(e.target.value)} style={{ width: '100%', height: '300px' }} />}
     </div>
   );
 }
