4b0c5d9e28db25bf815394ff00b8a87046812e99
Anthony Venturini <anthonydventurini@gmail.com>
2019-05-06 17:15:44 -0700

    Adds styling to the home profile page

--- src/components/dashboardContent/homeProfileContent/HouseProfileForms.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileForms.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { houseProfileData } from '../../../dummy-data-structures/house-profile-dummy-data';
+import { houseProfileDataFinishes } from '../../../dummy-data-structures/house-profile-finishes-dummy-data';
+import { houseProfileDataParcel } from '../../../dummy-data-structures/house-profile-parcel-dummy-data';
 import HouseProfileForm from './HouseProfileForm';
+import HouseProfileFinishes from './HouseProfileFinishes';
+import HouseProfileParcel from './HouseProfileParcel';
 
 function HouseProfileForms() {
   return (
@@ -10,7 +14,15 @@ function HouseProfileForms() {
           <HouseProfileForm key={index} title={i.title} content={i.content} />
         ))}
       </div>
-      <div className="house_profile_forms_right">Finish and Parcel Data</div>
+      <div className="house_profile_forms_right">
+      {houseProfileDataFinishes.map((i, index) => (
+          <HouseProfileFinishes key={index} title={i.title} countertops={i.countertops} furnaceAge={i.furnaceAge} roofAge={i.roofAge} acAge={i.acAge} />
+        ))}
+        {houseProfileDataParcel.map((i, index) => (
+          <HouseProfileParcel key={index} title={i.title} squareFt={i.squareFt} numBedrooms={i.numBedrooms} numBathrooms={i.numBathrooms} lotSize= {i.lotSize}/>
+        ))}
+          
+      </div>
     </div>
   );
 }
