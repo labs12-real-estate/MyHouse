f8b1cd151bb9b2c08c9ab11049ee23f17d63f217
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-07 13:51:06 -0700

    Added an upgradelist for mobile

---
+++ src/dummy-data-structures/valuation-playground-master-upgrade-list.js
@@ -0,0 +1,173 @@
+export const masterUpgradeList = [
+  { name: 'Select', upgrade_cost: null, ROI_percentage: null },
+  {
+    name: 'Install Laminate Countertops',
+    upgrade_cost: 1200,
+    ROI_percentage: 1.2
+  },
+  {
+    name: 'Install Granite Countertops',
+    upgrade_cost: 3250,
+    ROI_percentage: 1.35
+  },
+  {
+    name: 'Install Marble Countertops',
+    upgrade_cost: 3830,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Install Composite Roof',
+    upgrade_cost: 5500,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Metal Roof',
+    upgrade_cost: 6700,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Solar Roof',
+    upgrade_cost: 37145,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Install Clay Tile Roof',
+    upgrade_cost: 20000,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Cedar/Shake Roof',
+    upgrade_cost: 12700,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Stucco Siding',
+    upgrade_cost: 14000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Cedar/Shake Siding',
+    upgrade_cost: 12000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Vinyl Siding',
+    upgrade_cost: 8500,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Brick Siding',
+    upgrade_cost: 22500,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Wood Siding',
+    upgrade_cost: 6000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Fiber Cement Siding',
+    upgrade_cost: 9000,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Central AC',
+    upgrade_cost: 7200,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Install Ductless AC',
+    upgrade_cost: 2500,
+    ROI_percentage: 1.25
+  },
+  {
+    name: 'Install Window AC',
+    upgrade_cost: 455,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Electric Furnace',
+    upgrade_cost: 3000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Propane Furnace',
+    upgrade_cost: 4000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Oil-Fired Furnace',
+    upgrade_cost: 4000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Install Natural Gas Furnace',
+    upgrade_cost: 6000,
+    ROI_percentage: 1.25
+  },
+  {
+    name: 'Install Carpet',
+    upgrade_cost: 2500,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Install Tile Flooring',
+    upgrade_cost: 2000,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Install Laminate Flooring',
+    upgrade_cost: 3500,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Install Hardwood Flooring',
+    upgrade_cost: 5000,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Install Concrete Flooring',
+    upgrade_cost: 5000,
+    ROI_percentage: 1.6
+  },
+  {
+    name: 'Add a Loft',
+    upgrade_cost: 18000,
+    ROI_percentage: 1.1
+  },
+  {
+    name: 'Add a Furnished Basement',
+    upgrade_cost: 10800,
+    ROI_percentage: 1.25
+  },
+  {
+    name: 'Add a Bathroom',
+    upgrade_cost: 15000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Add a Bedroom',
+    upgrade_cost: 25000,
+    ROI_percentage: 1.0
+  },
+  {
+    name: 'Full Exterior Paint',
+    upgrade_cost: 3500,
+    ROI_percentage: 2.5
+  },
+  {
+    name: 'Full Interior Paint',
+    upgrade_cost: 3000,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Repaint Front Door',
+    upgrade_cost: 450,
+    ROI_percentage: 1.5
+  },
+  {
+    name: 'Replace Front Door',
+    upgrade_cost: 1500,
+    ROI_percentage: 2.5
+  }
+];
