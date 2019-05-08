a298e33741ad80d7b016f2a485ccf2af4744009d
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-08 10:36:33 -0700

    Changed way VPG accept data from backend

--- src/views/ValuationPlaygroundPage.js
+++ src/views/ValuationPlaygroundPage.js
@@ -1,26 +1,17 @@
-import React, { useEffect } from 'react';
-import { getUserHouse } from '../actions/usersActions';
+import React from 'react';
 import { connect } from 'react-redux';
 import DashboardContainer from './DashboardContainer';
 import ValuationPlaygroundContent from '../components/dashboardContent/valuationPlaygroundContent/ValuationPlaygroundContent';
 
-function ValuationPlaygroundPage({ error, house, username, getUserHouse }) {
-  useEffect(() => {
-    getUserHouse(username);
-    username !== '' && getUserHouse(username);
-  }, [getUserHouse, username]);
+function ValuationPlaygroundPage() {
   return (
     <DashboardContainer>
-      <ValuationPlaygroundContent {...house} />
+      <ValuationPlaygroundContent />
     </DashboardContainer>
   );
 }
 
 export default connect(
-  ({ houseReducer, authReducer }) => ({
-    house: houseReducer.house,
-    error: houseReducer.error,
-    username: authReducer.username
-  }),
-  { getUserHouse }
+  null,
+  {}
 )(ValuationPlaygroundPage);
