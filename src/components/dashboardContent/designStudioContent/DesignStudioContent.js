ab2ae6ad1cb895b1cda71737de75e08d3857b16b
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-08 08:25:26 -0700

    Renders buttons

--- src/components/dashboardContent/designStudioContent/DesignStudioContent.js
+++ src/components/dashboardContent/designStudioContent/DesignStudioContent.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Button from '../../buttons/Button';
+
+const searchTerms = ['Bedroom', 'Bathroom', 'Kitchen', 'Dining Room', 'Living Room', 'Office'];
 
 function DesignStudioContent() {
   return (
     <div className="dashboard_content_container">
       <h1>DesignStudioContent</h1>
+      {searchTerms.map(search => (
+        <Button buttonText={search} buttonStyle="design_studio_btn" clickEvent={() => {}} />
+      ))}
     </div>
   );
 }
