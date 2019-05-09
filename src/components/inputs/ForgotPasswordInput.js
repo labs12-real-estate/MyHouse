29e3b2c404160a5f066287578b8907741583f795
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-09 14:00:45 -0700

    completed functionality for ForgotPasswordInput

--- src/components/inputs/ForgotPasswordInput.js
+++ src/components/inputs/ForgotPasswordInput.js
@@ -1,37 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { useForm } from '../../helper-functions/form-logic-functions';
-import { toggleForgotPassword } from '../../actions/authActions';
-import Button from '../buttons/Button';
+import { toggleForgotPassword, forgotPassword } from '../../actions/authActions';
+import { loginModalButtonRender } from '../../helper-functions/display-functions';
 
 const initialCreds = {
   username: ''
 };
 
-function ForgotPasswordInput({ toggleForgotPassword }) {
+function ForgotPasswordInput({ toggleForgotPassword, fetching, forgotPassword }) {
   const [creds, handleChange] = useForm(initialCreds);
   return (
     <div>
       <h2>Forgot Password?</h2>
       <div className="login_gradient" />
-      <form>
+      <form onSubmit={e => forgotPassword(e, creds)}>
         <h3>Enter your username and we will e-mail you a confirmation code.</h3>
         <input placeholder="Username" name="username" value={creds.username} onChange={handleChange} className="login_input" type="text" />
-        <Button buttonStyle="modal_login_button" buttonText="Submit" />
+        {loginModalButtonRender(fetching, 'Submit', 'modal_login_button')}
         <p onClick={toggleForgotPassword}>Back</p>
       </form>
     </div>
   );
 }
 
-export default withRouter(
-  connect(
-    ({ authReducer: { fetching } }) => {
-      return {
-        fetching
-      };
-    },
-    { toggleForgotPassword }
-  )(ForgotPasswordInput)
-);
+export default connect(
+  ({ authReducer: { fetching } }) => {
+    return {
+      fetching
+    };
+  },
+  { toggleForgotPassword, forgotPassword }
+)(ForgotPasswordInput);
