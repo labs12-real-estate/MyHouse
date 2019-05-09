e44c6c00af46be411a6ed467b3c33dd3adc72f19
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-09 14:01:26 -0700

    Completed SetNewPasswordInput functionality

--- src/components/inputs/SetNewPasswordInput.js
+++ src/components/inputs/SetNewPasswordInput.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { useForm } from '../../helper-functions/form-logic-functions';
-import { toggleForgotPassword } from '../../actions/authActions';
-import Button from '../buttons/Button';
+import { confirmForgotPassword } from '../../actions/authActions';
+import { loginModalButtonRender } from '../../helper-functions/display-functions';
 
 const initialCreds = {
   username: '',
@@ -11,18 +11,18 @@ const initialCreds = {
   new_password: ''
 };
 
-function SetNewPasswordInput({ toggleForgotPassword }) {
+function SetNewPasswordInput({ fetching, confirmForgotPassword }) {
   const [creds, handleChange] = useForm(initialCreds);
   return (
     <div>
       <h2>Forgot Password?</h2>
       <div className="login_gradient" />
-      <form>
+      <form onSubmit={e => confirmForgotPassword(e, creds)}>
         <h3>Enter the below details to change your password.</h3>
         <input placeholder="Username" name="username" value={creds.username} onChange={handleChange} className="login_input" type="text" />
-        <input placeholder="Confirmation Code" name="code" value={creds.username} onChange={handleChange} className="login_input" type="text" />
-        <input placeholder="New Password" name="new_password" value={creds.username} onChange={handleChange} className="login_input" type="text" />
-        <Button buttonStyle="modal_login_button" buttonText="Submit" />
+        <input placeholder="Confirmation Code" name="code" value={creds.code} onChange={handleChange} className="login_input" type="text" />
+        <input placeholder="New Password" name="new_password" value={creds.new_password} onChange={handleChange} className="login_input" type="password" />
+        {loginModalButtonRender(fetching, 'Submit', 'modal_login_button_2')}
       </form>
     </div>
   );
@@ -35,6 +35,6 @@ export default withRouter(
         fetching
       };
     },
-    { toggleForgotPassword }
+    { confirmForgotPassword }
   )(SetNewPasswordInput)
 );
