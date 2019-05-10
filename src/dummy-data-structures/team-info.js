7c97496832023ceff66999cfcba20e03558b7ecb
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-10 13:22:35 -0700

    Added Jordan's info to team-info dummy data

--- src/dummy-data-structures/team-info.js
+++ src/dummy-data-structures/team-info.js
@@ -1,7 +1,7 @@
 export const teamInfo = [
   {
     name: 'Antony Venturini',
-    team: 'WEBTEAM',
+    team: 'Web Team',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
@@ -15,7 +15,7 @@ export const teamInfo = [
   },
   {
     name: 'David Lam',
-    team: 'WEBTEAM',
+    team: 'Web Team',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
@@ -28,7 +28,7 @@ export const teamInfo = [
   },
   {
     name: 'Jordan Marsaw',
-    team: 'PROJECT MANAGER',
+    team: 'Project Manager',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
@@ -41,20 +41,21 @@ export const teamInfo = [
   },
   {
     name: 'Jordan Stoddard',
-    team: 'WEBTEAM',
+    team: 'Web Team',
     intro:
-      'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
+      'Jordan Stoddard is a Full Stack Web Developer with 10 years experience in Project Management and Growth. \n In his previous career he had the opportunity to lead teams of 35+ and manage projects for some of the largest and well known companies in the world such as: Google, Bank of America, VMWare, Stanford University, IBM, Greylock Realty Partners and many others.',
     url: {
-      picture: 'https://picsum.photos/640/480',
-      twitter: '',
+      picture: 'https://zoom.us/p/jWk1V8RISV-TfsfmVaJXbQ/41d8979b-f997-407e-b965-4345974fc59c-1167?type=large',
+      twitter: 'https://twitter.com/Jdogstoddard',
       instagram: '',
-      github: '',
-      facebook: ''
+      github: 'https://github.com/Jordan-Stoddard',
+      facebook: '',
+      linkedin: 'https://www.linkedin.com/in/jordan-stoddard-aa928167/'
     }
   },
   {
     name: 'Gabe Samaniego',
-    team: 'WEBTEAM',
+    team: 'Web Team',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
@@ -67,7 +68,7 @@ export const teamInfo = [
   },
   {
     name: 'Peter Murphy',
-    team: 'WEBTEAM',
+    team: 'Web Team',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
@@ -80,7 +81,7 @@ export const teamInfo = [
   },
   {
     name: 'Peyton Runyan',
-    team: 'DATA SCIENCE',
+    team: 'Data Science Team',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
@@ -93,7 +94,7 @@ export const teamInfo = [
   },
   {
     name: 'Quinn Doughtery',
-    team: 'DATA SCIENCE',
+    team: 'Data Science Team',
     intro:
       'Lorem ipsum dolor sit amet, sumo idque deserunt usu an, vix graeco alterum te. Aperiri saperet te per. Nec utamur impedit at. Vim et nisl nemore mediocrem. Sea partem verterem eu, eros argumentum in cum. Iudico civibus eleifend pro eu.',
     url: {
