8b46becf238bb5a9537c3c6383dc6a5bffb04295
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-09 17:03:45 -0700

    Add file input in Backdrop component

--- src/components/dashboardContent/homeProfileContent/Backdrop.js
+++ src/components/dashboardContent/homeProfileContent/Backdrop.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Backdrop() {
+  const ref = useRef();
+  const [image, setImage] = React.useState(null);
+  const handleChange = e => {
+    const file = e.target.files[0];
+    const extension = file.type === 'image/jpeg' ? 'jpg' : 'png';
+    setImage(URL.createObjectURL(file));
+    console.log(`${Date.now()}.${extension}`);
+  };
+  const handleClick = e => {
+    ref.current.click();
+  };
   return (
     <div className="house_profile_backdrop_image">
-      <button>Upload Image</button>
+      {image && (
+        <figure className="backdrop_figure">
+          <img alt="" src={image} className="backdrop_img" />
+        </figure>
+      )}
+      <button onClick={handleClick}>Upload Image</button>
+      <input hidden type="file" accept="image/png, image/jpeg" ref={ref} onChange={handleChange} />
     </div>
   );
 }
