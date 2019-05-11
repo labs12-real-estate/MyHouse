1344bf5d865f62a0e97f91a8024f185623747cc6
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-11 09:57:49 -0700

    Infinite scroll works on all screens and removes duplicates.

--- src/components/dashboardContent/designStudioContent/ImageGallery.js
+++ src/components/dashboardContent/designStudioContent/ImageGallery.js
@@ -16,17 +16,18 @@ function ImageGallery({ searchResults, getNextPage, currentSearch, currentPage }
   };
   const [isFetching, setIsFetching] = useInfiniteScroll(getNext);
 
-  console.log('isFetching', isFetching);
+  const filteredImgs = Array.from(new Set(searchResults.map(JSON.stringify)))
+    .map(JSON.parse)
+    .filter(i => i.id !== '5GS27oQNFkI');
+
   return (
     <>
       {currentSearch === '' && <img src={decorateSvg} alt="" className="decorate_svg" />}
       <div className="design_studio_img_container">
         {searchResults &&
-          searchResults.map(result => {
+          filteredImgs.map(result => {
             return <ImageCard result={result} key={`${result.id}}`} />;
           })}
-
-        {/* {isFetching && <Loader height={50} width={50} type="TailSpin" color="#2868d9" />} */}
       </div>
     </>
   );
