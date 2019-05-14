1e967be08966585ad4bbcb79c86517323b0baba1
David Lam <Davidlamsgn@gmail.com>
2019-05-14 14:31:39 -0700

    bug fix

--- src/actions/landingpageActions.js
+++ src/actions/landingpageActions.js
@@ -25,3 +25,19 @@ export const getValuationv2 = (address, history) => dispatch => {
       dispatch({ type: GET_VALUATION_FAIL, payload: error });
     });
 };
+
+export const getValuation = (address, history) => dispatch => {
+  dispatch({ type: GET_VALUATION_FETCH });
+  return axios
+    .post('https://labs12-real-estate.herokuapp.com/api/houses/getvalue', {
+      address
+    })
+    .then(data => {
+      dispatch({ type: GET_VALUATION_SUCCESS });
+      localStorage.setItem('initialData', JSON.stringify(data));
+      history.push('/wizard-form');
+    })
+    .catch(error => {
+      dispatch({ type: GET_VALUATION_FAIL, payload: error });
+    });
+};
