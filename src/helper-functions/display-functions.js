81eca0edf59afd248e534239839207839c25925f
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-14 15:25:47 -0700

    Clearas warning caused by useInfiniteScroll

--- src/helper-functions/display-functions.js
+++ src/helper-functions/display-functions.js
@@ -47,21 +47,20 @@ export const useInfiniteScroll = callback => {
   const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
+    function handleScroll() {
+      if (Math.ceil(window.innerHeight + document.documentElement.scrollTop) !== Math.ceil(document.documentElement.offsetHeight) || isFetching) {
+        return;
+      }
+      setIsFetching(true);
+    }
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  }, [isFetching]);
 
   useEffect(() => {
     if (!isFetching) return;
     callback();
-  }, [isFetching]);
-
-  function handleScroll() {
-    if (Math.ceil(window.innerHeight + document.documentElement.scrollTop) !== Math.ceil(document.documentElement.offsetHeight) || isFetching) {
-      return;
-    }
-    setIsFetching(true);
-  }
+  }, [isFetching, callback]);
 
   return [isFetching, setIsFetching];
 };
