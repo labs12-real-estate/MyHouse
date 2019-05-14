e54f285979ec208c43b06adfac07b69edc0b672e
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-14 14:53:31 -0700

    Refactor Backdrop.js to use Redux

--- src/components/dashboardContent/homeProfileContent/Backdrop.js
+++ src/components/dashboardContent/homeProfileContent/Backdrop.js
@@ -1,28 +1,30 @@
 import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { uploadPhoto } from '../../../actions/storageActions';
 
-function Backdrop() {
+function Backdrop({ uploadPhoto, housePhotoURL }) {
   const ref = useRef();
-  const [image, setImage] = React.useState(null);
-  const handleChange = e => {
-    const file = e.target.files[0];
-    const extension = file.type === 'image/jpeg' ? 'jpg' : 'png';
-    setImage(URL.createObjectURL(file));
-    console.log(`${Date.now()}.${extension}`);
-  };
-  const handleClick = e => {
+  const handleClick = _event => {
     ref.current.click();
   };
   return (
     <div className="house_profile_backdrop_image">
-      {image && (
+      {housePhotoURL && (
         <figure className="backdrop_figure">
-          <img alt="" src={image} className="backdrop_img" />
+          <img alt="" src={housePhotoURL} className="backdrop_img" />
         </figure>
       )}
       <button onClick={handleClick}>Upload Image</button>
-      <input hidden type="file" accept="image/png, image/jpeg" ref={ref} onChange={handleChange} />
+      <input hidden name="house" type="file" accept="image/jpeg" ref={ref} onChange={uploadPhoto} />
     </div>
   );
 }
 
-export default Backdrop;
+const mapStateToProps = ({ storageReducer }) => ({
+  housePhotoURL: storageReducer.photoURLs.house
+});
+
+export default connect(
+  mapStateToProps,
+  { uploadPhoto }
+)(Backdrop);
