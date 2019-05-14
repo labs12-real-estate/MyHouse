8e842bb7c003d73052a8b60277fcfc2f46d322dc
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-14 16:00:41 -0700

    Bugfix: resolve errors from merge conflict

--- src/components/dashboardContent/homeProfileContent/HouseProfileParcel.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileParcel.js
@@ -20,69 +20,52 @@ function HouseProfileParcel({ saveHouseInfo, id, parcelData }) {
     setSelectedInput(null);
   };
 
-const parcelDataTitles = {
-	homeSquareFootage: 'Square Feet',
-	numBathrooms: 'Bathrooms',
-	numBedrooms: 'Bedrooms',
-	lotSquareFootage: 'Acres'
-};
-
-function HouseProfileParcel({ saveHouseInfo, id, parcelData }) {
-	const [ selectedInput, setSelectedInput ] = useState(null);
-	const inputSubmit = (k, v) => (e) => {
-		e.preventDefault();
-		const value = k === 'lotSquareFootage' ? acreToSqFtConversion(v) : v;
-		const newParcelData = {
-			...parcelData,
-			[k]: value
-		};
-		saveHouseInfo({ changes: { parcelData: newParcelData }, id });
-		setSelectedInput(null);
-	};
-
-	const inputBlur = (e) => {
-		const value = e.target.name === 'lotSquareFootage' ? acreToSqFtConversion(e.target.value) : e.target.value;
-		const newParcelData = {
-			...parcelData,
-			[e.target.name]: value
-		};
-		saveHouseInfo({ changes: { parcelData: newParcelData }, id });
-		setSelectedInput(null);
-	};
+  const inputBlur = e => {
+    const value = e.target.name === 'lotSquareFootage' ? acreToSqFtConversion(e.target.value) : e.target.value;
+    const newParcelData = {
+      ...parcelData,
+      [e.target.name]: value
+    };
+    saveHouseInfo({ changes: { parcelData: newParcelData }, id });
+    setSelectedInput(null);
+  };
 
   const handleSelect = key => _e => {
     setSelectedInput(key);
   };
 
-	return (
-		<div className="house_profile_form width">
-			<div className="house_profile_form_title">
-				<h2>Parcel Data</h2>
-			</div>
-			{Object.entries(parcelDataTitles).map(
-				([ key, value ]) =>
-					parcelData[key] && (
-						<label className="house_parcel_container" key={key} onClick={handleSelect(key)}>
-							{value}:{' '}
-							<HouseProfileParcelInput
-								initialValue={parcelData[key]}
-								inputSubmit={inputSubmit}
-								inputBlur={inputBlur}
-								name={key}
-								selected={selectedInput === key}
-							/>
-						</label>
-					)
-			)}
-		</div>
-	);
+  return (
+    <div className="house_profile_form width">
+      <div className="house_profile_form_title">
+        <h2>Parcel Data</h2>
+      </div>
+      {Object.entries(parcelDataTitles).map(
+        ([key, value]) =>
+          parcelData[key] && (
+            <label className="house_parcel_container" key={key} onClick={handleSelect(key)}>
+              {value}:{' '}
+              <HouseProfileParcelInput
+                initialValue={parcelData[key]}
+                inputSubmit={inputSubmit}
+                inputBlur={inputBlur}
+                name={key}
+                selected={selectedInput === key}
+              />
+            </label>
+          )
+      )}
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-	return {
-		id: state.houseReducer.house.id,
-		parcelData: state.houseReducer.house.parcelData
-	};
+const mapStateToProps = state => {
+  return {
+    id: state.houseReducer.house.id,
+    parcelData: state.houseReducer.house.parcelData
+  };
 };
 
-export default connect(mapStateToProps, { saveHouseInfo })(HouseProfileParcel);
+export default connect(
+  mapStateToProps,
+  { saveHouseInfo }
+)(HouseProfileParcel);
