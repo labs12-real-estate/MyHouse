ed0c8849e456db59e5189a6298c403f5534892d4
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-14 13:40:31 -0700

    call useWindowScrollY to determine if nav should be sticky

--- src/components/navigation/LandingTopBar.js
+++ src/components/navigation/LandingTopBar.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOut, openModal } from '../../actions/authActions';
+import { useWindowScrollY } from '../../helper-functions/display-functions';
 import Button from '../buttons/Button';
 import LoginModal from '../modals/LoginModal';
 
+const stickyStyle = {
+  position: 'sticky',
+  top: '0px',
+  backgroundImage: 'radial-gradient(931.35px at 85.76% 50%, #0045CB 0%, #0049D7 100%)',
+  zIndex: '900',
+  boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.25)'
+};
+
 function LandingTopBar({ isLoggedIn, signOut, openModal }) {
   return (
-    <div className="landing_nav_container">
+    <div style={useWindowScrollY() > 0 ? stickyStyle : {}} className="landing_nav_container">
       <div className="landing_nav_home_icon">
         <Link to="/">
           <i className="fas fa-home fa-2x" />
