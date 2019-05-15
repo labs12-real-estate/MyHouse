0bf373831b7952e0404197880818a65ca9726fa9
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-15 14:33:38 -0700

    Added a loader for valuation while fetching

--- src/components/jumbotrons/DashboardJumbo.js
+++ src/components/jumbotrons/DashboardJumbo.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
+import { connect } from 'react-redux';
 import { commaSeparator } from '../../helper-functions/display-functions';
 
-function DashboardJumbo({ address, valuation, houseBackgroundPhoto }) {
+function DashboardJumbo({ address, valuation, fetching }) {
   return (
     <div className="dashboard_jumbo">
       <div className="house_image" />
@@ -12,11 +14,18 @@ function DashboardJumbo({ address, valuation, houseBackgroundPhoto }) {
         </div>
         <div className="value">
           <h4>Valuation</h4>
-          <h1>{commaSeparator(valuation)}</h1>
+          <h1>{fetching ? <Loader height={25} width={25} type="TailSpin" color="#22ab00" /> : commaSeparator(valuation)}</h1>
         </div>
       </div>
     </div>
   );
 }
 
-export default DashboardJumbo;
+export default connect(
+  ({ houseReducer: { fetching } }) => {
+    return {
+      fetching
+    };
+  },
+  {}
+)(DashboardJumbo);
