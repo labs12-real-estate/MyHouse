078bd63706b656484a4aced8e9b758bfd0bfa725
Anthony Venturini <anthonydventurini@gmail.com>
2019-05-16 19:17:32 -0400

    cleans up some styles around app

--- src/components/dashboardContent/homeProfileContent/HouseProfileParcelInput.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileParcelInput.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { sqFtToAcreConversion } from '../../../helper-functions/display-functions';
+import { sqFtToAcreConversion, numberWithCommas } from '../../../helper-functions/display-functions';
 import { useInput } from '../../../helper-functions/form-logic-functions';
 
 export default function HouseProfileParcelInput({ initialValue: _initialValue, inputBlur, inputSubmit, name, selected }) {
-  const initialValue = name === 'lotSquareFootage' ? sqFtToAcreConversion(_initialValue) : _initialValue;
+  const initialValue = name === 'lotSquareFootage' ? sqFtToAcreConversion(_initialValue) : 'squareFootage' ? numberWithCommas(_initialValue) :  _initialValue;
   const [value, updateValue] = useInput(initialValue);
   return selected ? (
     <form onSubmit={inputSubmit(name, value)}>
-      <input  value={value} onChange={updateValue} type="number" min={ name === 'lotSquareFootage' ? 0 : 1 }maxLength='5' onBlur={inputBlur} name={name} step= { name === 'lotSquareFootage' ? .01 : name === 'numBathrooms' ? .1 : 1 }/>
+      <input  value={value} onChange={updateValue} type="number" min={ name === 'lotSquareFootage' ? 0 : 1 } maxLength='6' onBlur={inputBlur} name={name} step= { name === 'lotSquareFootage' ? .01 : name === 'numBathrooms' ? .1 : 1 }/>
     </form>
   ) : (
     <span className= 'house_parcel_input'>{value}</span>
