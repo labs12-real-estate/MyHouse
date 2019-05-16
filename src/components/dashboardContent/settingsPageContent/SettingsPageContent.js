483f7a2c4ef73a056f6a73387d2fab612593e8ee
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-15 17:37:10 -0700

    Add settings page content

--- src/components/dashboardContent/settingsPageContent/SettingsPageContent.js
+++ src/components/dashboardContent/settingsPageContent/SettingsPageContent.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { UserInfo, AttributesForm, NewPasswordForm } from './sections';
 
-function SettingsPageContent() {
+function SettingsPageContent({ user }) {
   return (
     <div className="dashboard_content_container">
       <h1 className="dashboard_content_title">Settings</h1>
+      <div className="settings_container">
+        <div className="settings_content">
+          <h2>Profile</h2>
+          <UserInfo />
+          {user.username && <AttributesForm />}
+          <h2>Security</h2>
+          <NewPasswordForm />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default SettingsPageContent;
+export default connect(({ authReducer }) => ({
+  user: authReducer.user
+}))(SettingsPageContent);
---
+++ src/components/dashboardContent/settingsPageContent/sections/AttributesForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { useForm, validateEmail } from 'helper-functions/form-logic-functions.js';
+import ErrorContainer from './ErrorContainer';
+
+function AttributesForm({ user }) {
+  const [formState, handleChange] = useForm({ name: user.name, email: user.email });
+  const [errorState, setErrorState] = useState({ email: 'Please enter a valid email' });
+  const handleSubmit = e => {
+    e.preventDefault();
+    setErrorState(errorState => ({ ...errorState, email: 'Please enter a valid email' }));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className="inline-grid">
+        Name
+        <input name="name" type="text" value={formState.name} onChange={handleChange} />
+      </label>
+      <ErrorContainer error={errorState.email} />
+      <label className="inline-grid">
+        Email
+        <input name="email" type="text" value={formState.email} onChange={handleChange} />
+      </label>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default connect(({ authReducer }) => ({
+  user: authReducer.user
+}))(AttributesForm);
