b0094bc6971af5a7857e126d97a7571a574d3b4d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-16 14:50:07 -0700

    Aligns form wiz buttons for desktop and tablet

--- src/components/wizardForm/WizardFormConfirmation.js
+++ src/components/wizardForm/WizardFormConfirmation.js
@@ -17,28 +17,30 @@ function WizardFormConfirmation({ getHouseInput, questions, handleAnswer, handle
   return (
     <div className="confirmation_container">
       <div className="review_container">
-        {questions.map(({ title, options, selected }, qIx) => (
-          <div className="selection" key={qIx}>
-            <Fragment>
-              <label htmlFor={title}>{title}</label>
-              <select onChange={handleChange(qIx)} value={selected === null ? NULL : selected} id={title}>
-                <option value={NULL}>Not applicable</option>
-                {options.map((opt, oIx) => (
-                  <option key={oIx} value={oIx}>
-                    {opt}
-                  </option>
-                ))}
-              </select>
-            </Fragment>
-          </div>
-        ))}
-        <p>Please register to get a more accurate valuation.</p>
-        <div className="button_container">
-          <Button clickEvent={handlePrev} buttonStyle="prev_button" buttonText="Prev" />
-          <Button clickEvent={openModal} buttonStyle="register_button" buttonText="Register" />
-          <RegisterModal getHouseInput={getHouseInput} />
+        <div className="upgrade_summary">
+          {questions.map(({ title, options, selected }, qIx) => (
+            <div className="selection" key={qIx}>
+              <Fragment>
+                <label htmlFor={title}>{title}</label>
+                <select onChange={handleChange(qIx)} value={selected === null ? NULL : selected} id={title}>
+                  <option value={NULL}>Not applicable</option>
+                  {options.map((opt, oIx) => (
+                    <option key={oIx} value={oIx}>
+                      {opt}
+                    </option>
+                  ))}
+                </select>
+              </Fragment>
+            </div>
+          ))}
         </div>
-        <div className="shadow" />
+        {/* <div className="shadow" /> */}
+        <p>Please register to get a more accurate valuation.</p>
+      </div>
+      <div className="button_container">
+        <Button clickEvent={handlePrev} buttonStyle="prev_button" buttonText="Prev" />
+        <Button clickEvent={openModal} buttonStyle="register_button" buttonText="Register" />
+        <RegisterModal getHouseInput={getHouseInput} />
       </div>
       <div className="left-panel">{/* <RegisterInput handlePrev={handlePrev} houseInput={getHouseInput()} /> */}</div>
     </div>
