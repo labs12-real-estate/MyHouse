986b510ff6ea8f081ade0267cc94d7415cbe49ab
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-17 12:00:24 -0700

    Add upload for profile photo on Settings page

--- src/components/dashboardContent/settingsPageContent/sections/UserInfo.js
+++ src/components/dashboardContent/settingsPageContent/sections/UserInfo.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import profilePhoto from 'assets/images/house-profile/tonystark.jpg';
+import React, { useRef } from 'react';
 import { connect } from 'react-redux';
+import profilePhoto from 'assets/images/house-profile/tonystark.jpg';
+import { uploadPhoto } from 'actions/storageActions';
 
-function UserInfo({ user }) {
+function UserInfo({ user, profilePhotoURL, uploadPhoto }) {
+  const ref = useRef();
+  const handleClick = _event => {
+    ref.current.click();
+  };
   return (
     <div className="inline-grid user-info">
-      <img src={profilePhoto} alt="" />
+      <figure>
+        <img src={profilePhotoURL || profilePhoto} alt="" />
+      </figure>
       <div>
         <h4>{user.username}</h4>
-        <button onClick={() => {}}>Change user profile photo</button>
+        <button onClick={handleClick}>Change user profile photo</button>
+        <input hidden name="profile" type="file" accept="image/jpeg" ref={ref} onChange={uploadPhoto} />
       </div>
     </div>
   );
 }
 
-export default connect(({ authReducer }) => ({
-  user: authReducer.user
-}))(UserInfo);
+export default connect(
+  ({ authReducer, storageReducer }) => ({
+    user: authReducer.user,
+    profilePhotoURL: storageReducer.photoURLs.profile
+  }),
+  { uploadPhoto }
+)(UserInfo);
