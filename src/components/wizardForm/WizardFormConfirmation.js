b43eb4570b98cc02abee03d7630c67d4ff920960
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-17 15:51:35 -0700

    form wizard styles complete and fully responsive

--- src/components/wizardForm/WizardFormConfirmation.js
+++ src/components/wizardForm/WizardFormConfirmation.js
@@ -15,35 +15,37 @@ function WizardFormConfirmation({ getHouseInput, questions, handleAnswer, handle
     handleAnswer(qIx)(oIx)(e);
   };
   return (
-    <div className="confirmation_container">
-      <div className="review_container">
-        <div className="upgrade_summary">
-          {questions.map(({ title, options, selected }, qIx) => (
-            <div className="selection" key={qIx}>
-              <Fragment>
-                <label htmlFor={title}>{title}</label>
-                <select onChange={handleChange(qIx)} value={selected === null ? NULL : selected} id={title}>
-                  <option value={NULL}>Not applicable</option>
-                  {options.map((opt, oIx) => (
-                    <option key={oIx} value={oIx}>
-                      {opt}
-                    </option>
-                  ))}
-                </select>
-              </Fragment>
-            </div>
-          ))}
+    <>
+      <div className="confirmation_container">
+        <div className="review_container">
+          <div className="upgrade_summary">
+            {questions.map(({ title, options, selected }, qIx) => (
+              <div className="selection" key={qIx}>
+                <Fragment>
+                  <label htmlFor={title}>{title}</label>
+                  <select onChange={handleChange(qIx)} value={selected === null ? NULL : selected} id={title}>
+                    <option value={NULL}>Not applicable</option>
+                    {options.map((opt, oIx) => (
+                      <option key={oIx} value={oIx}>
+                        {opt}
+                      </option>
+                    ))}
+                  </select>
+                </Fragment>
+              </div>
+            ))}
+          </div>
+          {/* <div className="shadow" /> */}
+          {/* <p>Please register to get a more accurate valuation.</p> */}
         </div>
-        {/* <div className="shadow" /> */}
-        {/* <p>Please register to get a more accurate valuation.</p> */}
-        <div className="button_container">
-          <Button clickEvent={handlePrev} buttonStyle="prev_button" buttonText="Prev" />
-          <Button clickEvent={openModal} buttonStyle="register_button" buttonText="Register" />
-          <RegisterModal getHouseInput={getHouseInput} />
-        </div>
+        <div className="left-panel">{/* <RegisterInput handlePrev={handlePrev} houseInput={getHouseInput()} /> */}</div>
       </div>
-      <div className="left-panel">{/* <RegisterInput handlePrev={handlePrev} houseInput={getHouseInput()} /> */}</div>
-    </div>
+      <div className="button_container">
+        <Button clickEvent={handlePrev} buttonStyle="prev_button" buttonText="Prev" />
+        <Button clickEvent={openModal} buttonStyle="register_button" buttonText="Register" />
+        <RegisterModal getHouseInput={getHouseInput} />
+      </div>
+    </>
   );
 }
 
