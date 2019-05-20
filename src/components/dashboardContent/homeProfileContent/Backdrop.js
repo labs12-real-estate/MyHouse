75d3ba4fd8e8ba249f54417d1b2608549957c055
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-20 13:47:08 -0700

    Made entire backdrop clickable

--- src/components/dashboardContent/homeProfileContent/Backdrop.js
+++ src/components/dashboardContent/homeProfileContent/Backdrop.js
@@ -8,7 +8,7 @@ function Backdrop({ uploadPhoto, housePhotoURL }) {
     ref.current.click();
   };
   return (
-    <div className="house_profile_backdrop_image">
+    <div className="house_profile_backdrop_image" onClick={handleClick}>
       {housePhotoURL && (
         <figure className="backdrop_figure">
           <img alt="" src={housePhotoURL} className="backdrop_img" />
@@ -16,7 +16,7 @@ function Backdrop({ uploadPhoto, housePhotoURL }) {
       )}
       {/* <button onClick={handleClick}>Upload Image</button> */}
       <div className="camera-overlay">
-        <button onClick={handleClick}>
+        <button>
           <i className="fas fa-camera" />
         </button>
       </div>
