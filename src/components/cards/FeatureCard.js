b0196eeaa8574eef84978a5a43affda99e93d3c2
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-20 09:38:43 -0700

    Added a loader spinner during fetching for Sq Ft in FeatureCard

--- src/components/cards/FeatureCard.js
+++ src/components/cards/FeatureCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 import { connect } from 'react-redux';
 import { LazyImage } from 'react-lazy-images';
 
@@ -13,7 +14,7 @@ function FeatureCard({ lazyUrl, url, text, value, fetching }) {
       />
       <div className="text_box">
         <h3>{text}</h3>
-        <h1>{value}</h1>
+        <h1>{fetching && text === 'SQUARE FEET' ? <Loader height={25} width={25} type="TailSpin" color="black" /> : value}</h1>
       </div>
     </div>
   );
