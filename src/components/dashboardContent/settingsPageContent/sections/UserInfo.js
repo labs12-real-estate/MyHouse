39fbe7e048014bc81d161cf41d6eed45fc2c130a
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-21 16:36:29 -0700

    New image for dafault backdrop - house profile page

--- src/components/dashboardContent/settingsPageContent/sections/UserInfo.js
+++ src/components/dashboardContent/settingsPageContent/sections/UserInfo.js
@@ -2,8 +2,8 @@ import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import profilePhoto from '../../../../assets/images/house-profile/tonystark.jpg';
 import { uploadPhoto } from '../../../../actions/storageActions';
-import profilePhoto from 'assets/images/house-profile/tonystark.jpg';
-import { uploadPhoto } from 'actions/storageActions';
+// import profilePhoto from 'assets/images/house-profile/tonystark.jpg';
+// import { uploadPhoto } from 'actions/storageActions';
 import Skeleton from 'react-loading-skeleton';
 
 function UserInfo({ user, profilePhotoURL, uploadPhoto, fetching }) {
