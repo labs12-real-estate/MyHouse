68117b6e02a9958e9fa140438348d1147df6c5dc
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-21 16:25:58 -0700

    Added error states for onBlur on parcelData

--- src/components/dashboardContent/homeProfileContent/HouseProfileParcelInput.js
+++ src/components/dashboardContent/homeProfileContent/HouseProfileParcelInput.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { sqFtToAcreConversion, numberWithCommas } from '../../../helper-functions/display-functions';
+import { sqFtToAcreConversion } from '../../../helper-functions/display-functions';
 import { useInput } from '../../../helper-functions/form-logic-functions';
 
 export default function HouseProfileParcelInput({ initialValue: _initialValue, inputBlur, inputSubmit, name, selected }) {
-  const initialValue = name === 'lotSquareFootage' ? sqFtToAcreConversion(_initialValue) :  _initialValue;
-  const [value, updateValue] = useInput(initialValue);
+  const initialValue = name === 'lotSquareFootage' ? sqFtToAcreConversion(_initialValue) : _initialValue;
+  const [value, updateValue, setValue] = useInput(initialValue);
   return selected ? (
     <form onSubmit={inputSubmit(name, value)}>
-      <input  value={value} onChange={updateValue} type='number' min={ name === 'lotSquareFootage' ? '0' : '1' } max='999999' onBlur={inputBlur} name={name} step= { name === 'lotSquareFootage' ? .01 : name === 'numBathrooms' ? .1 : 1 }/>
+      <input
+        value={value}
+        onChange={updateValue}
+        type="number"
+        max={'999999'}
+        onBlur={
+          value >= 0
+            ? inputBlur
+            : e => {
+                setValue(Math.abs(value));
+                inputBlur(e);
+              }
+        }
+        name={name}
+        step={name === 'lotSquareFootage' ? 0.01 : name === 'numBathrooms' ? 0.1 : 1}
+      />
     </form>
   ) : (
-    <span className= 'house_parcel_input'>{value}</span>
+    <span className="house_parcel_input">{value < 0 ? 'No negatives.' : name === 'lotSquareFootage' && value > 49000 ? 'Error: 49k+' : value}</span>
   );
 }
