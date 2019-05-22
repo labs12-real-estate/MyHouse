4e1597bedafcc8025ee234d94251cc34b17456d3
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-22 12:52:59 -0700

    Add ImageCard component

--- src/components/dashboardContent/homeProfileContent/Gallery.js
+++ src/components/dashboardContent/homeProfileContent/Gallery.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import { uploadToGallery, deleteFromGallery } from 'actions/storageActions';
 import { openGalleryModal } from 'actions/displayActions';
+import ImageCard from './ImageCard';
 
 function Gallery({ gallery, uploadToGallery, openGalleryModal, deleteFromGallery }) {
   const ref = useRef();
@@ -9,32 +10,35 @@ function Gallery({ gallery, uploadToGallery, openGalleryModal, deleteFromGallery
     ref.current.click();
   };
   const handleDeleteFromGallery = key => e => {
+    console.log('click');
     e.stopPropagation();
     deleteFromGallery(key);
   };
   return (
-    <div className="house_profile_gallery_container">
-      <h2>Gallery</h2>
-      <div className="gallery">
-        {gallery.length === 0 ? (
-          <h3>Add images to your gallery</h3>
-        ) : (
-          gallery.map(({ photoURL, key }) => (
-            <figure className="gallery-figure" key={key} onClick={() => openGalleryModal(photoURL)}>
-              <button className="trash" onClick={handleDeleteFromGallery(key)}>
-                <i className="fas fa-trash" />
-              </button>
-              <img alt="" src={photoURL} />
-            </figure>
-          ))
-        )}
+    <div className="house_profile_gallery_wrapper">
+      <div className="house_profile_gallery_container">
+        <h2>Gallery</h2>
+        <div className="gallery">
+          {gallery.length === 0 ? (
+            <h3>Add images to your gallery</h3>
+          ) : (
+            gallery.map(({ photoURL, key }) => (
+              <ImageCard
+                photoURL={photoURL}
+                key={key}
+                handleModalOpen={() => openGalleryModal(photoURL)}
+                handleDeleteFromGallery={handleDeleteFromGallery(key)}
+              />
+            ))
+          )}
+        </div>
+        <div className="button-container">
+          <button onClick={handleClick}>
+            <i className="fas fa-plus" /> Add an Image
+          </button>
+        </div>
+        <input hidden name="" type="file" accept="image/jpeg" ref={ref} onChange={uploadToGallery} />
       </div>
-      <div className="button-container">
-        <button onClick={handleClick}>
-          <i className="fas fa-plus" /> Add an Image
-        </button>
-      </div>
-      <input hidden name="" type="file" accept="image/jpeg" ref={ref} onChange={uploadToGallery} />
     </div>
   );
 }
