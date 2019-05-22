24e095085d37e6a69796193652d60fa1569484ac
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-21 19:13:34 -0700

    Add gallery modal states to display reducer

--- src/reducers/displayReducer.js
+++ src/reducers/displayReducer.js
@@ -1,11 +1,17 @@
-import { TOGGLE_SIDEBAR_DRAWER, CLOSE_SIDEBAR_DRAWER } from '../actions';
+import { TOGGLE_SIDEBAR_DRAWER, CLOSE_SIDEBAR_DRAWER, OPEN_GALLERY_MODAL, CLOSE_GALLERY_MODAL } from '../actions';
 
 const initialState = {
-  isOpen: false
+  isOpen: false,
+  galleryModalImageURL: '',
+  isGalleryModalOpen: false
 };
 
 export const displayReducer = (state = initialState, action) => {
   switch (action.type) {
+    case OPEN_GALLERY_MODAL:
+      return { ...state, isGalleryModalOpen: true, galleryModalImageURL: action.payload };
+    case CLOSE_GALLERY_MODAL:
+      return { ...state, isGalleryModalOpen: false, galleryModalImageURL: '' };
     case CLOSE_SIDEBAR_DRAWER:
       return { ...state, isOpen: false };
     case TOGGLE_SIDEBAR_DRAWER:
