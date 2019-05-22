95912ec846f7f0fe06459a964a93c39b5cc775ef
Peter Murphy <peter.francis.murphy@gmail.com>
2019-05-22 15:01:55 -0700

    Fix duplicate JSX from merge conflict

--- src/components/dashboardContent/homeProfileContent/Backdrop.js
+++ src/components/dashboardContent/homeProfileContent/Backdrop.js
@@ -24,26 +24,6 @@ function Backdrop({ uploadPhoto, housePhotoURL }) {
         <input hidden name="house" type="file" accept="image/jpeg" ref={ref} onChange={uploadPhoto} />
       </div>
     </>
-
-    <div className="house_profile_backdrop_image">
-      {housePhotoURL === null ? (
-        <Loader height={100} width={100} type="TailSpin" color="#22ab00" />
-      ) : (
-        <div className="house_profile_backdrop_image" onClick={handleClick}>
-          {housePhotoURL && (
-            <figure className="backdrop_figure">
-              <img alt="" src={housePhotoURL || placeholderBackdrop} className="backdrop_img" />
-            </figure>
-          )}
-          <div className="camera-overlay">
-            <button>
-              <i className="fas fa-camera" />
-            </button>
-          </div>
-          <input hidden name="house" type="file" accept="image/jpeg" ref={ref} onChange={uploadPhoto} />
-        </div>
-      )}
-    </div>
   );
 }
 
